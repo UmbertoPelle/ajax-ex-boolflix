e68b45e325e211954c5b43478a5d4aae36d38c37
Umberto <umbertopellegrino96@gmail.com>
2020-07-27 12:49:53 +0200

    Find and Print series

--- js/jscript.js
+++ js/jscript.js
@@ -1,9 +1,9 @@
 function findMovie() {
-  var apiKey = 'dc2cea832b9cc2420fe1b945e738abdf';
   var btn = $('#btnSerch');
-  var input = $('#serchInput');
 
   btn.click(function () {
+    var input = $('#serchInput');
+    var apiKey = 'dc2cea832b9cc2420fe1b945e738abdf';
     $('#containerMovies').text('');
     var request = input.val();
 
@@ -17,13 +17,59 @@ function findMovie() {
       },
       success : function (data) {
         var arrayMovie = data['results'];
-        printMovie(arrayMovie);
+        if (data['total_results'] >0) {
+          printMovie(arrayMovie);
+        }else {
+          findSerie();
+        }
       },
       error : function (err) {
         console.log(err,"error");
       }
     });
   });
+}
+
+function findSerie() {
+  var input = $('#serchInput');
+  var apiKey = 'dc2cea832b9cc2420fe1b945e738abdf';
+  $('#containerMovies').text('');
+  var request = input.val();
+
+  $.ajax({
+    url:'https://api.themoviedb.org/3/search/tv',
+    method:'GET',
+    data:{
+      'api_key':apiKey,
+      'query':request,
+      'language': 'it-IT'
+    },
+    success : function (data) {
+      var arraySeries = data['results'];
+      printSeries(arraySeries);
+    },
+    error : function (err) {
+      console.log(err,"error");
+    }
+  });
+}
+
+function printSeries(arraySeries) {
+  var template = $('#movie-template').html();
+  var compiled = Handlebars.compile(template)
+  var target = $('#containerMovies');
+
+  for (var i = 0; i < arraySeries.length; i++) {
+    var series = arraySeries[i];
+    var vote = Math.round(series['vote_average']/2);
+
+    var seriesHtml= compiled({
+      'title':series['name'],
+      'original_title':series['original_name'],
+      'original_language':series['original_language'],
+    });
+    target.append(seriesHtml);
+  }
 }
 
 function printMovie(arrayMovie) {
