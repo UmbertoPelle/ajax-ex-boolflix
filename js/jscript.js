c7d159618f6179297aa73e12e8b8c0657e7a1967
Umberto <umbertopellegrino96@gmail.com>
2020-07-27 15:58:47 +0200

    Milestone 3

--- js/jscript.js
+++ js/jscript.js
@@ -1,42 +1,45 @@
 function findMovie() {
-  var btn = $('#btnSerch');
 
-  btn.click(function () {
-    var input = $('#serchInput');
-    var apiKey = 'dc2cea832b9cc2420fe1b945e738abdf';
-    $('#containerMovies').text('');
-    var request = input.val();
+  $('#serchInput').keydown(function () {
+    if (event.which == 13) {
+      var apiKey = 'dc2cea832b9cc2420fe1b945e738abdf';
+      var input = $('#serchInput');
+      $('#containerMovies').text('');
+      var request = input.val();
 
-    if (request) {
-      $.ajax({
-        url:'https://api.themoviedb.org/3/search/movie',
-        method:'GET',
-        data:{
-          'api_key':apiKey,
-          'query':request,
-          'language': 'it-IT'
-        },
-        success : function (data) {
-          var arrayMovie = data['results'];
-          if (data['total_results'] >0) {
-            printMovie(arrayMovie);
-          }else {
-            findSerie();
+      if (request) {
+        $.ajax({
+          url:'https://api.themoviedb.org/3/search/movie',
+          method:'GET',
+          data:{
+            'api_key':apiKey,
+            'query':request,
+            'language': 'it-IT'
+          },
+          success : function (data) {
+            var arrayMovie = data['results'];
+            if (data['total_results'] >0) {
+              printMovie(arrayMovie);
+            }else {
+              findSerie();
+            }
+          },
+          error : function (err) {
+            console.log(err,"error");
           }
-        },
-        error : function (err) {
-          console.log(err,"error");
-        }
-      });
+        });
+      }
     }
-
   });
 }
 
 function printMovie(arrayMovie) {
+  $('#serchInput').val('');
   var template = $('#movie-template').html();
   var compiled = Handlebars.compile(template)
   var target = $('#containerMovies');
+  var poster = 'https://image.tmdb.org/t/p/w342';
+
 
   for (var i = 0; i < arrayMovie.length; i++) {
     var movie = arrayMovie[i];
@@ -46,6 +49,7 @@ function printMovie(arrayMovie) {
       'title':movie['title'],
       'original_title':movie['original_title'],
       'original_language':movie['original_language'],
+      'poster_path': poster + movie['poster_path']
     });
     target.append(movieHtml);
 
@@ -72,18 +76,27 @@ function findSerie() {
     },
     success : function (data) {
       var arraySeries = data['results'];
-      printSeries(arraySeries);
+      if (data['total_results'] >0) {
+        printSeries(arraySeries);
+      }else {
+        $('#containerMovies').text('NESSUN CONTENUTO TROVATO');
+      }
+
+
     },
     error : function (err) {
       console.log(err,"error");
     }
+
   });
 }
 
 function printSeries(arraySeries) {
+  $('#serchInput').val('');
   var template = $('#movie-template').html();
   var compiled = Handlebars.compile(template)
   var target = $('#containerMovies');
+  var poster = 'https://image.tmdb.org/t/p/w342'
 
   for (var i = 0; i < arraySeries.length; i++) {
     var series = arraySeries[i];
@@ -92,6 +105,7 @@ function printSeries(arraySeries) {
       'title':series['name'],
       'original_title':series['original_name'],
       'original_language':series['original_language'],
+      'poster_path': poster + series['poster_path']
     });
     target.append(seriesHtml);
     for (var x = 0; x <= vote; x++) {
@@ -100,10 +114,16 @@ function printSeries(arraySeries) {
   }
 }
 
-
+function home() {
+  $('.logoBoolFlix').click(function () {
+    $('#serch #serchInput').val('');
+    $('#containerMovies').text('');
+  });
+}
 
 function init() {
   findMovie();
+  home();
 }
 
 $(document).ready(init);
